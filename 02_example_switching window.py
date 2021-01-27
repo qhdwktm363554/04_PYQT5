357266dff1245c7ae2e50bf1def0faefdf76911b
qhdwktm363554 <qhdwktm3635@gmail.com>
2021-01-28 06:42:52 +0900

    210128_comma eliminated

--- 02_example_switching window.py
+++ 02_example_switching window.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow, 
+from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow
 
 class MainWindow(QMainWindow):
     def __init__(self):
