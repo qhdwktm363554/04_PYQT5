67f7d0467c977851945e918076b8cd5d48a92b9f
qhdwktm363554 <qhdwktm3635@gmail.com>
2021-01-28 06:34:06 +0900

    210128_first GUI_very simple

---
+++ 02_example_switching window.py
@@ -0,0 +1,41 @@
+import sys
+from PyQt5.uic import loadUi
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow, 
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        loadUi("screen1.ui", self)
+        self.pushButton.clicked.connect(self.gotoScreen2)
+    def gotoScreen2(self):
+        screen2 = Screen2()
+        widget.addWidget(screen2)
+
+        widget.setCurrentIndex(widget.currentIndex()+1)
+class Screen2(QDialog):
+    def __init__(self):
+        super(Screen2, self).__init__()
+        loadUi("screen2.ui", self)
+        self.pushButton.clicked.connect(self.gotoScreen1)
+    def gotoScreen1(self):
+        mainwindow = MainWindow()
+        widget.addWidget(mainwindow)
+        
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+app = QApplication(sys.argv)
+widget = QtWidgets.QStackedWidget()
+mainwindow = MainWindow()
+
+widget.addWidget(mainwindow)
+
+widget.setFixedHeight(300)
+widget.setFixedWidth(400)
+widget.show()
+
+
+try:
+    sys.exit(app.exec_())
+except:
+    print("Exiting")
